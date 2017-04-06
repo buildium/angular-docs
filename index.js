3a2047984d331b7692fd92836d7bf75496f655fb
Calvin McEachron <calvin.mceachron@buildium.com>
2017-04-06 11:24:35 -0400

    Avoid error from gh-pages dependency `collections` modifying Array.prototype

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 var generateDocs = require('./lib/generate-docs')
-var ghpages = require('gh-pages')
 var path = require('path')
 
-module.exports = function buildiumAngularDocs(options) {
+module.exports = function buildiumAngularDocs (options) {
   options.destination = options.destination || path.join(process.cwd(), 'docs')
   generateDocs(options)
 
   if (options.ghPages) {
+    // NOTE: this has to be after generateDocs()
+    // gh-pages has a dependency on https://github.com/montagejs/collections
+    // which modifies Array.prototype
+    var ghpages = require('gh-pages')
+
     ghpages.publish(options.destination, options, function (err) {
       if (err) {
-        process.stderr.write(err.message + '\n');
-        return process.exit(1);
+        process.stderr.write(err.message + '\n')
+        return process.exit(1)
       }
-      process.stderr.write('Published documentation\n');
+      process.stderr.write('Published documentation\n')
     })
   }
 }
